1e9eb1a49e460ecaf6feac765b4fe3c623f51c53
ummfieg <ummfieg@naver.com>
2024-09-11 09:31:05 +0900

    test3

---
+++ git-test02/MyButton.js
@@ -0,0 +1,8 @@
+
+// function MyButton() {
+//     return <h1>Hello World!</h1>;//문자열이 아닌 태그가 삽입되어 있다.
+//   }
+
+//   const container = document.getElementById('root');
+//   const root = ReactDOM.createRoot(container);
+//   root.render(<MyButton />) //root에 랜더링 한다. 함수검색을 사용자 정의 태그로 한다. 
